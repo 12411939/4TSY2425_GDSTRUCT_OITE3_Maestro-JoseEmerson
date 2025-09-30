062b12ca65016814a8d0579c0b5c1b48224a81e5
Maestro <joseemerson.maestro@benilde.edu.ph>
2025-09-30 20:41:57 +0800

    Quiz2
    Quiz 2 Linked List

--- ListQ2/src/Main.java
+++ ListQ2/src/Main.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        PlayerLinkedList playerLinkedList= new PlayerLinkedList();
+    public static void main(String[] args) {PlayerLinkedList playerLinkedList= new PlayerLinkedList();
         playerLinkedList.addToFront(new Player(1, "Saitama", 999));
         playerLinkedList.addToFront(new Player(2, "Jamal", 500));
         playerLinkedList.addToFront(new Player(3, "Goku", 100));
 
+        playerLinkedList.removeFirst();
         playerLinkedList.printLinkedList();
+        playerLinkedList.countLinkedList();
+
+        System.out.println(playerLinkedList.containsLinkedList(new Player(1, "Saitama", 999)));
+        System.out.println(playerLinkedList.indexOfPlayer(new Player(1,"Saitama", 999)));
     }
 }
--- ListQ2/src/PlayerLinkedList.java
+++ ListQ2/src/PlayerLinkedList.java
@@ -1,10 +1,12 @@
 public class PlayerLinkedList {
     private PlayerNode head;
+    private int count;
 
     public void addToFront(Player player) {
         PlayerNode newNode = new PlayerNode(player);
         newNode.setNextPlayerNode(head);
         head = newNode;
+        count++;
     }
 
     public void printLinkedList() {
@@ -17,6 +19,41 @@ public class PlayerLinkedList {
         }
 
         System.out.print("NULL");
+    }
+
+    public void removeFirst() {
+        if (head != null) {
+            head = head.getNextPlayerNode();
+            count--;
+        }
+    }
+
+    public void countLinkedList() {
+        System.out.print("\n" + count + "\n");
+    }
+
+    public boolean containsLinkedList(Player player) {
+        PlayerNode current = head;
+        while (current != null) {
+            if (current.getPlayer().equals(player)){
+                return true;
+            }
+            current = current.getNextPlayerNode();
+        }
+        return false;
+    }
+
+    public int indexOfPlayer(Player player) {
+        PlayerNode current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.getPlayer().equals(player)){
+                return index;
+            }
+            current = current.getNextPlayerNode();
+            index++;
+        }
+        return -1;
     }
 }
 
